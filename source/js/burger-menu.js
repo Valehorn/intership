8f743bc9817c44671072af3be41f789a0666ad5e
Пакри <pakri@yandex.ru>
2024-09-16 19:26:57 +0300

    Почти закончил реализацию бургер меню

--- source/js/burger-menu.js
+++ source/js/burger-menu.js
@@ -1,4 +1,4 @@
-import { isEscapeKey, isEnterKey } from './utils';
+import { isEscapeKey } from './utils';
 
 const body = document.querySelector('.page-body');
 const nav = document.querySelector('.header__nav');
@@ -7,14 +7,14 @@ const burgerButton = document.querySelector('.header__button-menu');
 const openMenu = () => {
   body.classList.add('page-body--menu-open');
   nav.classList.add('header__nav--open');
-  document.addEventListener('keydown', onDocumentEscKeyDown);
+  document.addEventListener('keydown', onDocumentKeyDown);
 };
 
 const closeMenu = () => {
   if (body.classList.contains('page-body--menu-open')) {
     body.classList.remove('page-body--menu-open');
     nav.classList.remove('header__nav--open');
-    document.removeEventListener('keydown', onDocumentEscKeyDown);
+    document.removeEventListener('keydown', onDocumentKeyDown);
   }
 };
 
@@ -31,20 +31,43 @@ const onBurgerButtonClick = () => {
   toggleMenu();
 };
 
-function onDocumentEscKeyDown(evt) {
+function onDocumentKeyDown(evt) {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeMenu();
   }
 }
 
-document.addEventListener('click', (evt) => {
+const onDocumentClick = (evt) => {
   const isClickNav = evt.target.closest('.header__nav');
   const isClickBurgerButton = evt.target.closest('.header__button-menu');
   if (!isClickNav && !isClickBurgerButton) {
     closeMenu();
   }
-});
+};
+
+const onNavButtonClick = (evt) => {
+  const button = evt.target.closest('.header__nav-button');
+  if (!button) {
+    return;
+  }
+  const parentItem = button.closest('.header__nav-item');
+  const subList = parentItem.querySelector('.header__nav-sub-list');
+
+  if (subList) {
+    if (subList.style.height && subList.style.height !== '0px') {
+      subList.style.height = '0px';
+      subList.style.visibility = 'hidden';
+    } else {
+      subList.style.height = `${subList.scrollHeight}px`;
+      subList.style.visibility = 'visible';
+    }
+  }
+};
+
+nav.addEventListener('click', onNavButtonClick);
+
+document.addEventListener('click', onDocumentClick);
 
 const burgerMenuToggle = () => {
   burgerButton.addEventListener('click', onBurgerButtonClick);
