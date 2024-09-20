0d4f4072ae90179dafd5c8b4cfa9accc9777d484
Пакри <pakri@yandex.ru>
2024-09-21 02:38:35 +0300

    Начал реализовывать свой select в модалке

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import { burgerMenuToggle } from './burger-menu';
 import { initSwiperHero } from './hero-swiper';
 import './modal';
-import './select';
+import './modal-select';
 import './validate';
 
 const bootStrap = () => {
---
+++ source/js/modal-select.js
@@ -0,0 +1,36 @@
+const modal = document.querySelector('.modal');
+const fields = modal.querySelector('.form-group__fields');
+const select = modal.querySelector('.form-group__select');
+const item = modal.querySelectorAll('.form-group__select-item');
+const modalItemContainer = modal.querySelector('.form-group__select-item-container');
+const selectValue = modal.querySelector('.modal__select-value');
+
+const onSelectClickOpen = () => {
+  if (fields.classList.contains('form-group__fields--open')) {
+    fields.classList.remove('form-group__fields--open');
+  } else {
+    fields.classList.add('form-group__fields--open');
+  }
+};
+
+const onSelectClickClose = () => {
+  fields.classList.remove('form-group__fields--open');
+};
+
+const onSelectItemClick = (evt) => {
+  if (!item) {
+    return;
+  }
+  selectValue.textContent = evt.target.dataset.cityModal;
+};
+
+select.addEventListener('click', onSelectClickOpen);
+select.addEventListener('blur', onSelectClickClose);
+
+modalItemContainer.addEventListener('click', onSelectItemClick);
+
+modal.addEventListener('click', (evt) => {
+  if (!select.contains(evt.target) && !fields.contains(evt.target)) {
+    fields.classList.remove('form-group__fields--open');
+  }
+});
