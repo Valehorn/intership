f58d9c5691a8809a295572b3cab64fd574809045
Пакри <pakri@yandex.ru>
2024-09-21 19:31:49 +0300

    Лок класс для кнопок свайпера в programs

--- source/js/programs-swiper.js
+++ source/js/programs-swiper.js
@@ -5,7 +5,7 @@ import 'swiper/scss';
 const programsSwiperContainer = document.querySelector('.programs__swiper');
 
 const initSwiperPrograms = () => {
-  new Swiper(programsSwiperContainer, {
+  const swiperPrograms = new Swiper(programsSwiperContainer, {
     modules: [Pagination],
     speed: 600,
     spaceBetween: 10,
@@ -29,8 +29,38 @@ const initSwiperPrograms = () => {
           clickable: true,
         }
       },
+    },
+    on: {
+      slideChange() {
+        updateNavigationButtons(swiperPrograms);
+      },
+      slideNextTransitionStart() {
+        updateNavigationButtons(swiperPrograms);
+      }
     }
   });
+  updateNavigationButtons(swiperPrograms);
+
+  function updateNavigationButtons(swiper) {
+    const prevButton = document.querySelector('.programs__swiper-button-prev');
+    const nextButton = document.querySelector('.programs__swiper-button-next');
+
+    if (swiper.isBeginning) {
+      prevButton.classList.add('swiper-button--disabled');
+      prevButton.disabled = true;
+    } else {
+      prevButton.classList.remove('swiper-button--disabled');
+      prevButton.disabled = false;
+    }
+
+    if (swiper.isEnd) {
+      nextButton.classList.add('swiper-button--disabled');
+      nextButton.disabled = true;
+    } else {
+      nextButton.classList.remove('swiper-button--disabled');
+      nextButton.disabled = false;
+    }
+  }
 };
 
 export default initSwiperPrograms;
