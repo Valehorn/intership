22bdbb6aa7e153c8d746738d1378f182d7fb46c3
Пакри <pakri@yandex.ru>
2024-09-23 14:35:16 +0300

    Улучшил swiper в programs

--- source/js/programs-swiper.js
+++ source/js/programs-swiper.js
@@ -4,13 +4,12 @@ import 'swiper/scss';
 const programsSwiperContainer = document.querySelector('.programs__swiper');
 
 const initSwiperPrograms = () => {
-  const swiperPrograms = new Swiper(programsSwiperContainer, {
+  const programsSwiper = new Swiper(programsSwiperContainer, {
     speed: 600,
     spaceBetween: 10,
     slidesPerView: 1,
     scrollbar: {
       el: '.programs__swiper-scrollbar',
-      clickable: true,
     },
     navigation: {
       nextEl: '.programs__swiper-button-next',
@@ -20,26 +19,31 @@ const initSwiperPrograms = () => {
       768: {
         slidesPerView: 2.13,
         spaceBetween: 30,
+        scrollbar: {
+          draggable: true,
+          dragEffect: 'slide',
+        }
       },
       1440: {
         slidesPerView: 3,
         spaceBetween: 32,
         allowTouchMove: false,
-        pagination: {
-          clickable: true,
+        scrollbar: {
+          draggable: true,
+          dragEffect: 'slide',
         }
       },
     },
-    on: {
-      slideChange() {
-        updateNavigationButtons(swiperPrograms);
-      },
-      slideNextTransitionStart() {
-        updateNavigationButtons(swiperPrograms);
-      }
-    }
   });
-  updateNavigationButtons(swiperPrograms);
+  programsSwiper.on('slideChange', () => {
+    updateNavigationButtons(programsSwiper);
+  });
+
+  programsSwiper.on('slideNextTransitionStart', () => {
+    updateNavigationButtons(programsSwiper);
+  });
+
+  updateNavigationButtons(programsSwiper);
 
   function updateNavigationButtons(swiper) {
     const prevButton = document.querySelector('.programs__swiper-button-prev');
