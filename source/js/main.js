8941c3a5e2e7106e203bc8e5f5e32a63f2932ac7
Пакри <pakri@yandex.ru>
2024-10-01 15:12:04 +0300

    Переполнение в header и hero

--- source/js/main.js
+++ source/js/main.js
@@ -10,6 +10,12 @@ import initSwiperNews from './news-swiper';
 import { toggleFaqAccordion } from './faq-accordion';
 import initSwiperReviews from './reviews-swiper';
 
+const click = (evt) => {
+  console.log(evt.target);
+};
+
+document.addEventListener('click', click);
+
 const bootStrap = () => {
   burgerMenuToggle();
   initSwiperHero();
