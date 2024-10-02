9455341453b438656c9edcca81feaa7e99b5d26a
Пакри <pakri@yandex.ru>
2024-10-02 14:47:29 +0300

    Select с работающим фокусом в modal

--- source/js/modal-select.js
+++ source/js/modal-select.js
@@ -1,25 +1,27 @@
 const modal = document.querySelector('.modal');
 const modalFields = modal.querySelector('.form-group__fields');
 const modalSelect = modal.querySelector('.form-group__select');
-const modalItems = modal.querySelectorAll('.form-group__select-item');
 const modalItemContainer = modal.querySelector('.form-group__select-item-container');
+const modalItems = modal.querySelectorAll('.form-group__select-item');
 const modalSelectValue = modal.querySelector('.form-group__select-value');
 const modalHiddenSelectInput = modal.querySelector('.form-group__select-hidden');
 
-const onSelectClickOpen = () => {
+const onSelectClickToggle = () => {
   if (modalFields.classList.contains('form-group__fields--open')) {
     modalFields.classList.remove('form-group__fields--open');
+    modalItems.forEach((item) => {
+      item.setAttribute('tabindex', -1);
+    });
+    modalItemContainer.removeListener('click', onSelectItemClick);
+    modalItemContainer.removeListener('keydown', onSelectItemKeydownEnter);
   } else {
     modalFields.classList.add('form-group__fields--open');
+    modalItems.forEach((item) => {
+      item.setAttribute('tabindex', 0);
+    });
     modalItemContainer.addEventListener('click', onSelectItemClick);
     modalItemContainer.addEventListener('keydown', onSelectItemKeydownEnter);
   }
-};
-
-const onSelectClickClose = () => {
-  modalFields.classList.remove('form-group__fields--open');
-  modalItemContainer.removeEventListener('click', onSelectItemClick);
-  modalItemContainer.removeEventListener('keydown', onSelectItemKeydownEnter);
 };
 
 function onSelectItemClick(evt) {
@@ -28,10 +30,11 @@ function onSelectItemClick(evt) {
   }
   modalSelectValue.textContent = evt.target.dataset.cityModal;
   modalHiddenSelectInput.value = modalSelectValue.textContent.trim();
+  modalFields.classList.remove('form-group__fields--open');
 }
 
 function onSelectItemKeydownEnter(evt) {
-  if (!modalItems && !evt.key === 'Enter') {
+  if (evt.key !== 'Enter') {
     return;
   }
   modalSelectValue.textContent = evt.target.dataset.cityModal;
@@ -39,17 +42,4 @@ function onSelectItemKeydownEnter(evt) {
   modalFields.classList.remove('form-group__fields--open');
 }
 
-const onSelectKeydownEnter = (evt) => {
-  if (!modalSelect && !evt.key === 'Enter') {
-    return;
-  }
-  if (!modalFields.classList.contains('form-group__fields--open')) {
-    modalFields.classList.add('form-group__fields--open');
-  } else {
-    modalFields.classList.remove('form-group__fields--open');
-  }
-};
-
-modalSelect.addEventListener('keydown', onSelectKeydownEnter);
-modalSelect.addEventListener('click', onSelectClickOpen);
-modalSelect.addEventListener('blur', onSelectClickClose);
+modalSelect.addEventListener('click', onSelectClickToggle);
